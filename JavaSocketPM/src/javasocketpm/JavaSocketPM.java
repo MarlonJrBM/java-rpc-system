7aa9448358bcaff88c5f6dd95751ed19394bb0f3
Marlon Marques <marlonbr1@gmail.com>
2014-05-07 21:44:23 -0300

    Mais u teste

--- JavaSocketPM/src/javasocketpm/JavaSocketPM.java
+++ JavaSocketPM/src/javasocketpm/JavaSocketPM.java
@@ -17,6 +17,7 @@ public class JavaSocketPM {
      */
     public static void main(String[] args) {
         Object object = new Object();
+        String hello = "Hey Rodrigo!";
         // TODO code application logic here
     }
     
