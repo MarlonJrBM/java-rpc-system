3802ce98ef8b2c352766bb0aa061da53912d4d84
Marlon Marques <marlonbr1@gmail.com>
2014-05-08 09:06:49 -0300

    começa implementação de naming

---
+++ JavaSocketPM/src/naming/Naming.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package naming;
+
+import stubs.*;
+
+/**
+ *
+ * @author marlon
+ */
+public class Naming {
+   
+    
+    /**
+     * 
+     * @param url Uma url para o cliente se conectar ao servidor
+     * @return uma referência ao objeto remoto (Stub)
+     */
+    public static Stub lookup(String url)
+    {
+        
+        
+        return null;
+    }
+    
+    
+    /**
+     * 
+     * @param name nome identificador do objeto remoto
+     * @param stub uma referência para o objeto remoto (uma stub) 
+     */
+    public static void bind (String name, Object stub)
+    {
+        
+    }
+    
+}
